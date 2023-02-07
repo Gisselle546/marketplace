d8fe01a8dc28abbb2d57a9bede8207f95ae2c027
Gisselle Rodriguez <gissellerodriguez431@gmail.com>
2023-02-07 11:09:45 -0500

    update to footer and homepage styles

--- client/src/components/Footer/Footer.style.tsx
+++ client/src/components/Footer/Footer.style.tsx
@@ -3,8 +3,33 @@ import styled, {css} from 'styled-components'
 export const FooterContainer = styled.div(
     ({ theme: {color} }) => css`
     background: ${color.badgeBackground};
-    height:7rem;
+    height:16rem;
     display:flex;
-    
+    justify-content:space-around;
+    flex-direction: column;
+    align-items: center;
+
+    `
+)
+
+export const LogoContainer = styled.div`
+display: flex;
+cursor: pointer;
+
+@media screen and (max-width: 800px) {
+  padding: 0;
+  opacity: 1;
+}
+`
+export const NavItems = styled.div`
+display: flex;
+width: 100%;
+justify-content:center;
+
+`
+export const CopyRight = styled.div(
+    ({ theme: {color} }) => css`
+     backgound: ${color.screenBackground};
+     height: 2rem;
     `
 )
--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,10 +1,54 @@
 import React from 'react'
-import { FooterContainer } from './Footer.style'
+import styled, {css} from 'styled-components'
+import { FooterContainer, LogoContainer, NavItems, CopyRight } from './Footer.style'
+import Image from 'next/image';
+import logo from '../../assets/images/logo.png';
+
+export const LinkText = styled.a`
+ text-decoration: none;
+ margin-right: 0.3rem;
+`;
+
+const Spacing = styled.div`
+margin-bottom: 4.5rem;
+`
 
 function Footer() {
   return (
     <FooterContainer>
       
+        
+          <LogoContainer>
+            <Image
+            alt="logo"
+            src={logo}
+            width={80}
+            height={80}
+            style={{height:'99%'}}
+          />
+        </LogoContainer>
+        
+        
+      <NavItems>
+
+        <div>
+          <h3>Contact Us</h3>
+          <p>Buy a Home</p>
+          <p>Rent a Home</p>
+          
+        </div>
+        
+        <div style={{marginLeft:'6rem'}}>
+        <h3>About Us</h3>
+        <p>The Brand</p>
+        <p>Our Mission</p>
+        </div>
+        
+       </NavItems>
+       <Spacing/>
+       <CopyRight>&copy; 2023 All Rights Reserved.</CopyRight>
+     
+
     </FooterContainer>
   )
 }
