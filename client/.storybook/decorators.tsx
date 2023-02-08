458373e548730899704088365b1c05195a622902
Gisselle Rodriguez <gissellerodriguez431@gmail.com>
2023-02-07 19:37:35 -0500

    updates to storybook

---
+++ client/.storybook/decorators.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import styled, {css, ThemeProvider } from 'styled-components';
+import { DecoratorFn } from '@storybook/react';
+
+import { breakpoints } from '../src/styles/breakpoints';
+import { GlobalStyle } from '../src/styles/GlobalStyle';
+import { lightTheme, darkTheme } from '../src/styles/theme';
+
+const ThemeBlock = styled.div<{ left?: boolean; fullScreen?: boolean }>(
+    ({ left, fullScreen, theme: { color } }) =>
+      css`
+        position: absolute;
+        top: 0;
+        left: ${left ? 0 : '50vw'};
+        border-right: ${left ? '1px solid #202020' : 'none'};
+        right: ${left ? '50vw' : 0};
+        width: 50vw;
+        height: 100vh;
+        bottom: 0;
+        overflow: auto;
+        padding: ${fullScreen ? 0 : '1rem'};
+        background: ${color.screenBackground};
+        ${breakpoints.S} {
+          left: ${left ? 0 : '50vw'};
+          right: ${left ? '50vw' : 0};
+          padding: 0 !important;
+        }
+      `
+  )
+
+  export const withTheme: DecoratorFn = (StoryFn, { globals: { theme = 'light' }, parameters }) => {
+    const fullScreen = parameters.layout === 'fullscreen'
+    const appTheme = theme === 'light' ? lightTheme : darkTheme
+    const secondContainerRef = React.useRef<HTMLDivElement>(null)
+  
+    const firstBlockRef = React.useCallback(
+      (node) => {
+        if (node) {
+          node.addEventListener('scroll', () => {
+            if (secondContainerRef.current) {
+              secondContainerRef.current.scrollTop = node.scrollTop
+            }
+          })
+        }
+      },
+      [secondContainerRef.current]
+    )
+
+    switch (theme) {
+        case 'side-by-side': {
+            return (
+              <>
+                <ThemeProvider theme={lightTheme}>
+                  <GlobalStyle />
+                  <ThemeBlock ref={firstBlockRef} left fullScreen={fullScreen}>
+                    <StoryFn />
+                  </ThemeBlock>
+                </ThemeProvider>
+                <ThemeProvider theme={darkTheme}>
+                  <GlobalStyle />
+                  <ThemeBlock ref={secondContainerRef} fullScreen={fullScreen}>
+                    <StoryFn />
+                  </ThemeBlock>
+                </ThemeProvider>
+              </>
+            )
+          }
+          default: {
+            return (
+              <ThemeProvider theme={appTheme}>
+                <GlobalStyle />
+                <StoryFn />
+              </ThemeProvider>
+            )
+          }
+    }
+  }
+
+  export const globalDecorators = [withTheme]
--- client/.storybook/preview.ts
+++ client/.storybook/preview.ts
@@ -1,3 +1,22 @@
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { globalDecorators } from "./decorators";
+import { viewports as breakpoints } from '../src/styles/breakpoints';
+
+
+const breakpointViewports = Object.keys(breakpoints).reduce((acc, key) => {
+  acc[`breakpoint${key}`] = {
+    name: `Breakpoint - ${key}`,
+    styles: {
+      width: `${breakpoints[key as keyof typeof breakpoints]}px`,
+      // Account for padding and border around viewport preview
+      height: 'calc(100% - 20px)',
+    },
+    type: 'other',
+  }
+  return acc
+}, {} as typeof INITIAL_VIEWPORTS)
+
+
 export const parameters = {
   backgrounds: {
     default: 'light',
@@ -10,3 +29,21 @@ export const parameters = {
     },
   },
 }
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Theme for the components',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'circlehollow',
+      items: [
+        { value: 'light', icon: 'circlehollow', title: 'light' },
+        { value: 'dark', icon: 'circle', title: 'dark' },
+        { value: 'side-by-side', icon: 'sidebar', title: 'side by side' },
+      ],
+    },
+  },
+}
+
+export const decorators = globalDecorators
