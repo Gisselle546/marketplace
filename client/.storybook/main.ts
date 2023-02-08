bdfb66b3e793959fcc6603e7f1a7de2a3d37471b
Gisselle Rodriguez <gissellerodriguez431@gmail.com>
2023-02-07 19:15:40 -0500

    updated storybook

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -1,33 +1,21 @@
-const path = require('path')
+import type { StorybookConfig } from '@storybook/nextjs';
 
-module.exports = {
-  stories: [
-    '../src/docs/Introduction.stories.mdx',
-    '../src/docs/*.stories.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-a11y',
-    '@storybook/addon-coverage',
-    'storybook-addon-designs',
-    'storybook-mobile',
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
   ],
-  babel: async (options) => {
-    return {
-      ...options,
-      plugins: [...options.plugins, 'babel-plugin-open-source'],
-    }
+  "framework": {
+    "name": "@storybook/nextjs",
+    "options": {}
   },
-  staticDirs: ['../public'],
-  features: {
-    storyStoreV7: true,
-    interactionsDebugger: true,
-  },
-  framework: '@storybook/react',
-  core: {
-    builder: 'webpack5',
-  },
-}
+  "docs": {
+    "autodocs": "tag"
+  }
+};
+export default config;
