a1b84fff5780dfa332ccfa9dd4427fdb615d3e18
Gisselle Rodriguez <gissellerodriguez431@gmail.com>
2023-02-15 10:40:35 -0500

    updated ui

--- client/src/components/HomepageSection/HomepageSection.tsx
+++ client/src/components/HomepageSection/HomepageSection.tsx
@@ -3,6 +3,8 @@ import { HomepageSectionWrapper } from './HomepageSection.style'
 import styled, {css} from 'styled-components'
 import img from '../../assets/images/Homepagesection.jpg';
 import Button from '../Button/Button';
+import { useAppSelector } from '@/redux/hooks';
+import { selectValue } from '@/redux/reducer/register';
 
 
 const Backgroundimage = styled.div(
@@ -14,11 +16,14 @@ const Backgroundimage = styled.div(
 )
 
 function HomepageSection() {
+  const count = useAppSelector(selectValue);
   return (
     <HomepageSectionWrapper>
         <div>
             <h2> Find your perfect home</h2>
-            <Button>Sign Up Today</Button>
+            {
+              count? <Button> Check Homes Near You</Button>: <Button>Sign Up Today</Button>
+            }
         </div>
         <Backgroundimage/>
     </HomepageSectionWrapper>
