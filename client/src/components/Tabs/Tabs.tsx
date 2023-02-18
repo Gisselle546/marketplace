28b9ebfe2adc1c2542fc2e0033f1b2ba01e7651f
Gisselle Rodriguez <gissellerodriguez431@gmail.com>
2023-02-18 13:27:07 -0500

    tabs updated

--- client/src/components/Tabs/Tabs.tsx
+++ client/src/components/Tabs/Tabs.tsx
@@ -1,19 +1,29 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, {css} from 'styled-components'
 
 const TabsContainer = styled.div`
 height: 40rem;
 margin-top: 1rem; 
 border-radius: 5%; 
 display: flex; 
-justify-content: center; 
 box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
 `;
+
+const TabsHeader = styled.div(
+    ({ theme: {color} }) => css`
+display: flex;
+height: 5rem;
+width: 100%;
+border-bottom: 1px solid ${color.buttonSecondary};
+`
+)
 
 function Tabs() {
   return (
     <TabsContainer>
-        Tabs
+        <TabsHeader>
+            Tabs
+        </TabsHeader>
     </TabsContainer>
   )
 }
